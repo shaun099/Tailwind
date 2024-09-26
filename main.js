07c2ec1d3833dadd054a76cfc5d3104fd155a935
Shaun MR <shaunmr009@gmail.com>
2024-09-26 07:28:04 +0530

    website-demo

---
+++ main.js
@@ -0,0 +1,26 @@
+/*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("left-[0]");
+  hamburger.classList.toggle("ri-close-large-line");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]");
+    hamburger.classList.toggle("ri-close-large-line");
+  });
+});
+
+/*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+
+/*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
